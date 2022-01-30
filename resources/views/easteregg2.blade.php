39c6612574704f3d6e6dddbc775a1d0816aa0da4
Simon PERRIN <tox6mon@gmail.com>
2022-01-30 04:20:23 +0100

    EE2

--- resources/views/easteregg2.blade.php
+++ resources/views/easteregg2.blade.php
@@ -7,13 +7,13 @@ Muertissima - EASTER MF EGG
 @section('content')
 <div class="row mt-2">
     <div class="col text-center">
-        <img src="{{ asset('images/maka.jpg') }}" width="30%" />
+        <img src="{{ asset('images/maka2.jpg') }}" width="30%" />
     </div>
 </div>
 <div class="row mt-2 pb-2">
     <div class="col text-center">
         Salut à toi, chère gourgandine !<br />
-        Tu es la première à trouver cet Easter Egg ? Tu viens de gagner un rencart avec le chanteur 🥰<br />
+        Tu es la première à trouver cet Easter Egg ? Tu viens de gagner un rencard avec le chanteur 🥰<br />
         Cette soirée se terminera chez toi, ou chez moi, mais pas les deux à la fois.<br />
         Apportes-moi une preuve au prochain concert, et je paie ma bière 🍻😘
     </div>
